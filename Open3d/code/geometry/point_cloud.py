d9860343450985e3abb85954e894ef65c5c74c62
Yue Liu <41297969+YueLiu-coder@users.noreply.github.com>
2021-01-22 10:06:20 +0800

    add the point_cloud.py

--- Open3d/code/geometry/point_cloud.py
+++ Open3d/code/geometry/point_cloud.py
@@ -1,5 +1,6 @@
 import open3d as o3d
 import numpy as np
+import matplotlib.pyplot as plt
 # visualize point cloud
 pcd = o3d.io.read_point_cloud("./test_data/input.ply")
 print(pcd)
@@ -36,3 +37,49 @@ obb.color = (0, 1, 0)
 o3d.visualization.draw_geometries([pcd, aabb, obb])
 
 # convex hull
+hull, _ = pcd.compute_convex_hull()
+hull_ls = o3d.geometry.LineSet.create_from_triangle_mesh(hull)
+hull_ls.paint_uniform_color((1, 0, 0))
+o3d.visualization.draw_geometries([pcd, hull_ls])
+
+# DBSCAN clustering
+with o3d.utility.VerbosityContextManager(
+        o3d.utility.VerbosityLevel.Debug) as cm:
+    labels = np.array(
+        downpcd.cluster_dbscan(eps=5, min_points=10, print_progress=True))
+
+max_label = labels.max()
+print(f"point cloud has {max_label + 1} clusters")
+colors = plt.get_cmap("tab20")(labels / (max_label if max_label > 0 else 1))
+colors[labels < 0] = 0
+downpcd.colors = o3d.utility.Vector3dVector(colors[:, :3])
+o3d.visualization.draw_geometries([downpcd])
+
+# Plane segmentation
+plane_model, inliers = downpcd.segment_plane(distance_threshold=0.01,
+                                         ransac_n=3,
+                                         num_iterations=1000)
+[a, b, c, d] = plane_model
+print(f"Plane equation: {a:.2f}x + {b:.2f}y + {c:.2f}z + {d:.2f} = 0")
+
+inlier_cloud = downpcd.select_by_index(inliers)
+inlier_cloud.paint_uniform_color([1.0, 0, 0])
+outlier_cloud = downpcd.select_by_index(inliers, invert=True)
+o3d.visualization.draw_geometries([inlier_cloud, outlier_cloud])
+
+# Hidden point removal
+print("Convert mesh to a point cloud and estimate dimensions")
+diameter = np.linalg.norm(
+    np.asarray(pcd.get_max_bound()) - np.asarray(pcd.get_min_bound()))
+o3d.visualization.draw_geometries([pcd])
+
+print("Define parameters used for hidden_point_removal")
+camera = [0, 0, diameter]
+radius = diameter * 100
+
+print("Get all points that are visible from given view point")
+_, pt_map = pcd.hidden_point_removal(camera, radius)
+
+print("Visualize result")
+pcd = pcd.select_by_index(pt_map)
+o3d.visualization.draw_geometries([pcd])
