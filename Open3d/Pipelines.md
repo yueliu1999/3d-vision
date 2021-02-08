fe338b9160d5c385ff231d4fa372de5882e6205f
Yue Liu <41297969+YueLiu-coder@users.noreply.github.com>
2021-02-08 13:18:50 +0800

    add the global registration and multiway registration

--- Open3d/Pipelines.md
+++ Open3d/Pipelines.md
@@ -549,9 +549,181 @@ Q.-Y. Zhou, J. Park, and V. Koltun, Fast Global Registration, ECCV, 2016.
 
 ### Multiway registraion
 
+多路注册，
+
+输入是一个几何的集合，比如点云或者是RGBD images $\{ P_i\}$
+
+输出是一个rigid transformation$ \{ {T_i}\}$
+
+可以通过计算出转换后的点云可以align到一起
+
+- Input
+
+  从文件中读入三个点云
+
+  点云需要进行下采样和可视化
+
+  
+
+- Pose graph
+
+  位姿图 pose graph，
+
+  有两个元素
+
+  - nodes
+
+    是一个几何，比如点云
+
+  - edges
+
+    是一个transformation，讲source align到target
+
+    是一个point-to-plane ICP
+
+  
+
+  两两注册会发生错误，错误对齐的数量可能会超过正确对齐的数量
+
+  因此可以将位姿图分为两部分
+
+  - Odometry edges
+
+    连接比较接近相邻的节点，通过local registration 比如ICP来进行align
+
+  - Loop closure edges
+
+    连接任意的节点，可以不相邻，通过global registration 进行align
+
+  除了transformation，用户可以设置一个information matrix 为每一个edge
+
+  
+
+  构建pose graph图：
+
+  ```python
+  def pairwise_registration(source, target):
+      print("Apply point-to-plane ICP")
+      icp_coarse = o3d.pipelines.registration.registration_icp(
+          source, target, max_correspondence_distance_coarse, np.identity(4),
+          o3d.pipelines.registration.TransformationEstimationPointToPlane())
+      icp_fine = o3d.pipelines.registration.registration_icp(
+          source, target, max_correspondence_distance_fine,
+          icp_coarse.transformation,
+          o3d.pipelines.registration.TransformationEstimationPointToPlane())
+      transformation_icp = icp_fine.transformation
+      information_icp = o3d.pipelines.registration.get_information_matrix_from_point_clouds(
+          source, target, max_correspondence_distance_fine,
+          icp_fine.transformation)
+      return transformation_icp, information_icp
+  
+  
+  def full_registration(pcds, max_correspondence_distance_coarse,
+                        max_correspondence_distance_fine):
+      pose_graph = o3d.pipelines.registration.PoseGraph()
+      odometry = np.identity(4)
+      pose_graph.nodes.append(o3d.pipelines.registration.PoseGraphNode(odometry))
+      n_pcds = len(pcds)
+      for source_id in range(n_pcds):
+          for target_id in range(source_id + 1, n_pcds):
+              transformation_icp, information_icp = pairwise_registration(
+                  pcds[source_id], pcds[target_id])
+              print("Build o3d.pipelines.registration.PoseGraph")
+              if target_id == source_id + 1:  # odometry case
+                  odometry = np.dot(transformation_icp, odometry)
+                  pose_graph.nodes.append(
+                      o3d.pipelines.registration.PoseGraphNode(
+                          np.linalg.inv(odometry)))
+                  pose_graph.edges.append(
+                      o3d.pipelines.registration.PoseGraphEdge(source_id,
+                                                               target_id,
+                                                               transformation_icp,
+                                                               information_icp,
+                                                               uncertain=False))
+              else:  # loop closure case
+                  pose_graph.edges.append(
+                      o3d.pipelines.registration.PoseGraphEdge(source_id,
+                                                               target_id,
+                                                               transformation_icp,
+                                                               information_icp,
+                                                               uncertain=True))
+      return pose_graph
+  ```
+
+  ```python
+  print("Full registration ...")
+  max_correspondence_distance_coarse = voxel_size * 15
+  max_correspondence_distance_fine = voxel_size * 1.5
+  with o3d.utility.VerbosityContextManager(
+          o3d.utility.VerbosityLevel.Debug) as cm:
+      pose_graph = full_registration(pcds_down,
+                                     max_correspondence_distance_coarse,
+                                     max_correspondence_distance_fine)
+  ```
+
+  优化pose graph
+
+  BA
+
+  可以选择算法
+
+  - GlobalOptimizationGaussNewton
+  - GlobalOptimizationLevenbergMarquardt
+
+  
+
+  ```python
+  print("Optimizing PoseGraph ...")
+  option = o3d.pipelines.registration.GlobalOptimizationOption(
+      max_correspondence_distance=max_correspondence_distance_fine,
+      edge_prune_threshold=0.25,
+      reference_node=0)
+  with o3d.utility.VerbosityContextManager(
+          o3d.utility.VerbosityLevel.Debug) as cm:
+      o3d.pipelines.registration.global_optimization(
+          pose_graph,
+          o3d.pipelines.registration.GlobalOptimizationLevenbergMarquardt(),
+          o3d.pipelines.registration.GlobalOptimizationConvergenceCriteria(),
+          option)
+  ```
+
+  
+
+  
+
+- Visualize optimization
+
+  ```python
+  print("Transform points and display")
+  for point_id in range(len(pcds_down)):
+      print(pose_graph.nodes[point_id].pose)
+      pcds_down[point_id].transform(pose_graph.nodes[point_id].pose)
+  o3d.visualization.draw_geometries(pcds_down,
+                                    zoom=0.3412,
+                                    front=[0.4257, -0.2125, -0.8795],
+                                    lookat=[2.6172, 2.0475, 1.532],
+                                    up=[-0.0694, -0.9768, 0.2024])
+  ```
+
+- Make a combined point cloud
+
 
 
 ### RGBD integration
+
+
+
+### RGBD Odometry
+
+
+
+### Color Map Optimization
+
+
+
+
+
+
 
 
 
---
+++ Open3d/code/Pipelines/multiway_registration.py
@@ -0,0 +1,121 @@
+import open3d as o3d
+import numpy as np
+
+# load the point clouds and down sample
+def load_point_clouds(voxel_size=0.0):
+    pcds = []
+    for i in range(3):
+        pcd = o3d.io.read_point_cloud("../test_data/icp/cloud_bin_%d.pcd" % i)
+        pcd_down = pcd.voxel_down_sample(voxel_size)
+        pcds.append(pcd_down)
+
+    return pcds
+
+# pose graph and full registration
+voxel_size = 0.02
+pcds_down = load_point_clouds(voxel_size)
+o3d.visualization.draw_geometries(pcds_down,
+                                  zoom=0.3412,
+                                  front=[0.4257, -0.2125, -0.8795],
+                                  lookat=[2.6172, 2.0475, 1.532],
+                                  up=[-0.0694, -0.9768, 0.2024])
+
+
+# calculate the transformation from two point clouds with point-to-plane ICP
+def pairwise_registration(source, target):
+    print("Apply point-to-plane ICP")
+    icp_coarse = o3d.pipelines.registration.registration_icp(
+        source, target, max_correspondence_distance_coarse, np.identity(4),
+        o3d.pipelines.registration.TransformationEstimationPointToPlane())
+    icp_fine = o3d.pipelines.registration.registration_icp(
+        source, target, max_correspondence_distance_fine,
+        icp_coarse.transformation,
+        o3d.pipelines.registration.TransformationEstimationPointToPlane())
+    transformation_icp = icp_fine.transformation
+    information_icp = o3d.pipelines.registration.get_information_matrix_from_point_clouds(
+        source, target, max_correspondence_distance_fine,
+        icp_fine.transformation)
+    return transformation_icp, information_icp
+
+
+# full registration
+def full_registration(pcds, max_correspondence_distance_coarse,
+                      max_correspondence_distance_fine):
+    # 构建pose图
+    pose_graph = o3d.pipelines.registration.PoseGraph()
+    odometry = np.identity(4)
+    pose_graph.nodes.append(o3d.pipelines.registration.PoseGraphNode(odometry))
+    n_pcds = len(pcds)
+    for source_id in range(n_pcds):
+        for target_id in range(source_id + 1, n_pcds):
+            transformation_icp, information_icp = pairwise_registration(
+                pcds[source_id], pcds[target_id])
+            print("Build o3d.pipelines.registration.PoseGraph")
+            if target_id == source_id + 1:  # odometry case
+                odometry = np.dot(transformation_icp, odometry)
+                pose_graph.nodes.append(
+                    o3d.pipelines.registration.PoseGraphNode(
+                        np.linalg.inv(odometry)))
+                pose_graph.edges.append(
+                    o3d.pipelines.registration.PoseGraphEdge(source_id,
+                                                             target_id,
+                                                             transformation_icp,
+                                                             information_icp,
+                                                             uncertain=False))
+            else:  # loop closure case
+                pose_graph.edges.append(
+                    o3d.pipelines.registration.PoseGraphEdge(source_id,
+                                                             target_id,
+                                                             transformation_icp,
+                                                             information_icp,
+                                                             uncertain=True))
+    return pose_graph
+
+
+print("Full registration ...")
+max_correspondence_distance_coarse = voxel_size * 15
+max_correspondence_distance_fine = voxel_size * 1.5
+with o3d.utility.VerbosityContextManager(
+        o3d.utility.VerbosityLevel.Debug) as cm:
+    pose_graph = full_registration(pcds_down,
+                                   max_correspondence_distance_coarse,
+                                   max_correspondence_distance_fine)
+
+# refine the pose graph
+print("Optimizing PoseGraph ...")
+option = o3d.pipelines.registration.GlobalOptimizationOption(
+    max_correspondence_distance=max_correspondence_distance_fine,
+    edge_prune_threshold=0.25,
+    reference_node=0)
+with o3d.utility.VerbosityContextManager(
+        o3d.utility.VerbosityLevel.Debug) as cm:
+    o3d.pipelines.registration.global_optimization(
+        pose_graph,
+        o3d.pipelines.registration.GlobalOptimizationLevenbergMarquardt(),
+        o3d.pipelines.registration.GlobalOptimizationConvergenceCriteria(),
+        option)
+
+# visualize optimization
+print("Transform points and display")
+for point_id in range(len(pcds_down)):
+    print(pose_graph.nodes[point_id].pose)
+    pcds_down[point_id].transform(pose_graph.nodes[point_id].pose)
+o3d.visualization.draw_geometries(pcds_down,
+                                  zoom=0.3412,
+                                  front=[0.4257, -0.2125, -0.8795],
+                                  lookat=[2.6172, 2.0475, 1.532],
+                                  up=[-0.0694, -0.9768, 0.2024])
+
+# merge the point clouds
+pcds = load_point_clouds(voxel_size)
+pcd_combined = o3d.geometry.PointCloud()
+for point_id in range(len(pcds)):
+    pcds[point_id].transform(pose_graph.nodes[point_id].pose)
+    pcd_combined += pcds[point_id]
+pcd_combined_down = pcd_combined.voxel_down_sample(voxel_size=voxel_size)
+o3d.io.write_point_cloud("multiway_registration.pcd", pcd_combined_down)
+o3d.visualization.draw_geometries([pcd_combined_down],
+                                  zoom=0.3412,
+                                  front=[0.4257, -0.2125, -0.8795],
+                                  lookat=[2.6172, 2.0475, 1.532],
+                                  up=[-0.0694, -0.9768, 0.2024])
